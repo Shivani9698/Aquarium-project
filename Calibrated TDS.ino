2e99bbaab7d5fea0c802e1ab8212ce0f8bd478d6
Shivani9698 <119753029+Shivani9698@users.noreply.github.com>
2024-08-13 12:02:50 +0530

    Create Calibrated TDS.ino

---
+++ Calibrated TDS.ino
@@ -0,0 +1,15 @@
+void loop() {
+    int analogValue = analogRead(TdsSensorPin);  // Read the analog value from the TDS sensor
+    
+    if (analogValue > 10) {  // Ignore very low readings
+        float tdsValue = 0.3101 * analogValue + 15.12;
+        Serial.print("Analog Value: ");
+        Serial.println(analogValue);
+        Serial.print("TDS: ");
+        Serial.println(tdsValue);
+    } else {
+        Serial.println("Sensor is dry or not connected.");
+    }
+
+    delay(1000);  // Delay 1 second before reading the next value
+}
